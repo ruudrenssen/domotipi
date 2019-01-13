750d27a9a6f9c5067315738ef42a2781152d137d
Ruud <ruud.renssen@gmail.com>
2019-01-13 23:25:03 +0100

    render form based on lights

--- python/app.py
+++ python/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect
-from wtforms import Form, FormField, FieldList, StringField
+from wtforms import Form, FormField, FieldList, StringField, BooleanField, IntegerField, HiddenField
 from kodi import KodiRemote
 from hue import Hue
 
@@ -13,7 +13,12 @@ class KodiForm(Form):
 
 
 class LightForm(Form):
+    light_id = HiddenField()
     name = StringField()
+    on = BooleanField()
+    brightness = IntegerField()
+    saturation = IntegerField()
+    hue = IntegerField()
 
 
 class HueForm(Form):
@@ -21,20 +26,76 @@ class HueForm(Form):
 
 
 @app.route('/')
-def index(hue_data=hue):
-    light_fields = []
+def index():
+    light_info_fields = []
     for light in hue.lights:
-        light_field = {'name': light.name}
-        light_fields.append(light_field)
-    form = HueForm(hue_action=light_fields)
-    print(form.hue_action.data)
+        light_field = {}
+
+        type_value = getattr(light, 'type')
+        light_field['type'] = type_value
+
+        if type_value == 'Extended color light':
+            properties = ['light_id',
+                          'name',
+                          'on',
+                          'brightness',
+                          'colormode',
+                          'hue',
+                          'xy',
+                          'colortemp',
+                          'effect',
+                          'alert',
+                          'reachable']
+            light_info_fields.append(create_fields(light, light_field, properties))
+
+        if type_value == 'Color temperature light':
+            properties = ['light_id',
+                          'name',
+                          'on',
+                          'brightness',
+                          'colormode',
+                          'colortemp',
+                          'alert',
+                          'reachable']
+            light_info_fields.append(create_fields(light, light_field, properties))
+
+        if type_value == 'Dimmable light':
+            properties = ['light_id',
+                          'name',
+                          'on',
+                          'brightness',
+                          'alert',
+                          'reachable']
+            light_info_fields.append(create_fields(light, light_field, properties))
+
+        # light_id_value = getattr(light, 'light_id', None)
+        # name_value = getattr(light, 'name', None)
+        # on_value = getattr(light, 'on', None)
+        # brightness_value = getattr(light, 'brightness', None)
+        # colormode_value = getattr(light, 'colormode', None)
+        # hue_value = getattr(light, 'heu', None)
+        # xy_value = getattr(light, 'xy', None)
+        # colortemp_value = getattr(light, 'colortemp', None)
+        # effect_value = getattr(light, 'effect', None)
+        # alert_value = getattr(light, 'alert', None)
+        # reachable_value = getattr(light, 'reachable', None)
+
+        # light_field = {'light_id': light.light_id, 'name': light.name, 'brightness': light.brightness}
+        # light_field[attribute] = light[attribute]
+
+        # light_info_fields.append(light_field)
+    form = HueForm(hue_action=light_info_fields)
     return render_template('room.html', form=form.hue_action)
+
+
+def create_fields(light_obj, field_obj, properties):
+    for light_property in properties:
+        field_obj[light_property] = getattr(light_obj, light_property)
+    return field_obj
 
 
 @app.route('/hue', methods=['POST', 'GET'])
 def hue_action():
-    form = HueForm(request.form)
-    print(form.hue_action.data)
     return redirect("/", code=302)
 
 
