0a0c21fb5f3d2822006269077d173d7269af3ff1
Ruud <ruud.renssen@gmail.com>
2019-01-20 04:07:28 +0100

    colormode

--- python/hue.py
+++ python/hue.py
@@ -14,21 +14,28 @@ class Hue():
         self.lights = self.bridge.lights
 
     def transition_to_bright(self, light, seconds):
-        self.bridge.set_light(light.name, 'bri', 125, transitiontime = (seconds * 10))
+        self.bridge.set_light(light.name, 'bri', 125, transitiontime=(seconds * 10))
 
     def process_form(self, form):
         form = form.copy()
         light_id = int(form.pop('light_id').strip('light_'))
 
         if 'on' in form:
-            print(form['on'])
             form['on'] = bool(distutils.util.strtobool(form['on']))
-            print(form['on'])
-
-        if 'colortemp' in form:
+        elif 'colortemp' in form:
             form['colortemp'] = int(form['colortemp'])
+        elif 'brightness' in form:
+            form['brightness'] = int(form['brightness'])
+        elif 'colormode' in form:
+            if form['colormode'] == 'xy':
+                setattr(self.lights[light_id - 1], 'xy', getattr(self.lights[light_id - 1], 'xy'))
+                return
+            elif form['colormode'] == 'hs':
+                setattr(self.lights[light_id - 1], 'hue', getattr(self.lights[light_id - 1], 'hue'))
+                return
+            elif form['colormode'] == 'ct':
+                setattr(self.lights[light_id - 1], 'colortemp', getattr(self.lights[light_id - 1], 'colortemp'))
+                return
 
-        for property in form:
-            # print(property)
-            # print(form[property])
-            setattr(self.lights[light_id-1], property, form[property])
+        for light_property in form:
+            setattr(self.lights[light_id-1], light_property, form[light_property])
